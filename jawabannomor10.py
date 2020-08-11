cf353d6d21291b9a21709ac223abc4c22f12ea85
aweprasetya <69502168+aweprasetya@users.noreply.github.com>
2020-08-11 14:42:58 +0700

    Add files via upload

---
+++ jawabannomor10.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+def main():
+    main_url = "http://shopee.co.id"
+    r1 = requests.get(main_url)
+    soup = BeautifulSoup(r1.text, 'html.parser')
+    r1_script_array = soup.find_all('script')
+    
+    all_script = []
+    for item in r1_script_array:
+        if item.get('src'):
+            all_script.append(item.get('src'))
+        
+    for script_js in all_script:
+        t = requests.get(script_js).text
+        print(t)
+
+if __name__=="__main__":
+    main()
